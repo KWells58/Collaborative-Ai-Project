0af40be0d1579f39aa881fbd87e8e7f4ca952155
KWells58 <143289793+KWells58@users.noreply.github.com>
2023-09-28 21:53:45 -0400

    Update script.js
    Toggle Dark light

--- Virtual Assistant App/script.js
+++ Virtual Assistant App/script.js
@@ -3,6 +3,8 @@ const body = document.body;
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 
 //event listener for dark mode toggle
-darkModeToggle.addEventListener('click', ()=> {
+    darkModeToggle.addEventListener('click', ()=> {
     body.classList.toggle('dark-mode');
+    //Toggle between "Dark Mode and Light Mode" text
+    darkModeToggle.textContent = body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
 })

43ca220728690d7356cba0466d3d69217b029045
KWells58 <143289793+KWells58@users.noreply.github.com>
2023-09-28 22:09:40 -0400

    Update script.js
    Added sendMessage function to script.js to do things

--- Virtual Assistant App/script.js
+++ Virtual Assistant App/script.js
@@ -1,3 +1,4 @@
+
 //toggle darkmode
 const body = document.body;
 const darkModeToggle = document.getElementById('dark-mode-toggle');
@@ -8,3 +9,37 @@ const darkModeToggle = document.getElementById('dark-mode-toggle');
     //Toggle between "Dark Mode and Light Mode" text
     darkModeToggle.textContent = body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
 })
+
+
+    const userMessageInput = document.getElementById('user-message');
+    const chatContainer = document.querySelector('.chat');
+
+    function sendMessage() {
+        const messageText = userMessageInput.value;
+
+        //check if msg is empty
+        if(messageText.trim() !== '') {
+            //create a new message element
+            const messageElement = document.createElement('div');
+            messageElement.classList.add('chat-message', 'user');
+            messageElement.textContent = messageText;
+
+            //append the message to the chat container
+            chatContainer.appendChild(messageElement);
+
+            //clear the input field
+            userMessageInput.value = '';
+
+            //ai responses
+        }
+    }
+
+    //event listener
+    document.querySelector('button').addEventListener('click', sendMessage);
+
+    //event listener for sending a message when enter key is pressed
+   userMessageInput.addEventListener('keypress', (event)=> {
+       if(event.key === 'Enter') {
+           sendMessage();
+       }
+   })

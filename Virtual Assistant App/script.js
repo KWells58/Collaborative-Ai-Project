30e1bd12e154da6a6a8a15eb8b7203c2fd1b0c0b
KWells58 <143289793+KWells58@users.noreply.github.com>
2023-09-28 20:12:02 -0400

    Update script.js
    removed openai stuff and added darkmode toggle

--- Virtual Assistant App/script.js
+++ Virtual Assistant App/script.js
@@ -1,17 +1,8 @@
-import { OpenAI } from "langchain/llms/openai";
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { HumanMessage } from "langchain/schema";
+//toggle darkmode
+const body = document.body;
+const darkModeToggle = document.getElementById('dark-mode-toggle');
 
-const llms = new OpenAI
-({
-    openAIApiKey: "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"
-});
-
-const chatModel = new ChatOpenAI();
-
-const searchBox = document.querySelector(".search input");
-const messages = [new HumanMessage({ content: searchBox})];
-
-const response = await llms.predictMessages(messages);
-
-console.log(response);
+//event listener for dark mode toggle
+darkModeToggle.addEventListener('click', ()=> {
+    body.classList.toggle('dark-mode');
+})
